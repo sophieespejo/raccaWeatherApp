6fc0c16a06c219f658e0e19ef9c2030506d20f10
sophieespejo <sophmeas@gmail.com>
2022-01-20 21:42:39 -0800

    made responsive for 1200px screens

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,13 @@
 import { data, forecastData, fetchCurrentData, fetch5DayForecast, longitude, latitude, currentTemp, weatherDesc, currentMin, currentMax, cityName, currentTime, day1MaxTemp, day1MinTemp, day1OverallTemp, day2MaxTemp, day2MinTemp, day2OverallTemp, day3MaxTemp, day3MinTemp, day3OverallTemp, day4MaxTemp, day4MinTemp, day4OverallTemp, day5MaxTemp, day5MinTemp, day5OverallTemp, fetchedData } from "./fetches.js";
 // fetchCurrentData();
-let location = 'stockton';
+
 
 // fetchCurrentData(location);
 
-let openBtn = document.getElementById('openBtn');
-let closeBtn = document.getElementById('closeBtn');
-
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "200px";
-//   }
-  
-//   function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//   }
+let openBtn = document.getElementById('openBtn'),
+    closeBtn = document.getElementById('closeBtn'),
+    citySearch = document.getElementById('citySearch'),
+    searchBtn = document.getElementById('searchBtn');
 
   openBtn.addEventListener('click', function(e){
     document.getElementById("mySidenav").style.width = "350px";
@@ -21,4 +15,8 @@ let closeBtn = document.getElementById('closeBtn');
 
   closeBtn.addEventListener('click', function(e){
     document.getElementById("mySidenav").style.width = "0px";
+  })
+
+  searchBtn.addEventListener('click', function(e){
+      fetchCurrentData(citySearch.value);
   })
